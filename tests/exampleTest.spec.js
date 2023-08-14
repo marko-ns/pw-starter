f1580125bf955b2c54d5851895172f96ab755638
marko-ns <marko96jovic@gmail.com>
2023-08-14 18:20:00 +0200

    Test commit

--- tests/exampleTest.spec.js
+++ tests/exampleTest.spec.js
@@ -3,5 +3,5 @@ import { test } from '../fixtures'
 test('Example Test', async ({ page, isMobile, examplePage, beforeTest, afterTest }) => {
     await examplePage.goToPage()
     await examplePage.search('This is during test.')
-    await page.waitForTimeout(4000)
+    await page.waitForTimeout(4000) //test
 })
